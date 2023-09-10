4d1628c2d7d2bbb17f5ca3dad13e04ad9ba85b08
Bruno Forcato <bruno_hebeler@hotmail.com>
2023-09-10 05:45:55 -0300

    Melhora o dash

--- Assets/Player/SC_Moviment/SC_Moviment.cs
+++ Assets/Player/SC_Moviment/SC_Moviment.cs
@@ -1,66 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class SC_Moviment : MonoBehaviour
 {
-[Header("Movimentacao do jogador")]
+
+private TrailRenderer TracoDaEsquiva;
+
+    [Header("Movimentacao")]
     private new Rigidbody2D rigidbody2D;
     public float movimentoJogador = 4f;
     public float forcaPuloJogador = 500f;
     private bool taNoChao;
 
-[Header("Esquiva do jogador")]
-    public float duracaoEsquiva = 0.5f;
-    public float velocidadeEsquiva = 10f;
-    private bool estaEsquiva = false;
+    [Header("Esquiva")]
+    [SerializeField] private float velocidadeEsquiva = 14f;
+    [SerializeField] private float tempoEsquiva = 0.5f;
+    private Vector2 direcaoEsquiva;
+    private bool esquivaAtiva;
+    private bool podeEsquivar = true; 
 
-public void Start()
+
+    public void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>(); 
-
+        TracoDaEsquiva = GetComponent<TrailRenderer>();
+    }
+    
+    public void Update() {
+        Esquivar();
     }
 
-public void FixedUpdate() 
-{
-    float inputHorizontal = Input.GetAxisRaw("Horizontal");
+    public void FixedUpdate() 
     {
+        Mover();
+        Pular();
+    }
+
+    public void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Chao") 
+        {
+            taNoChao = true;
+        }
+    }
+
+    public void Esquivar() {
+       var botaoEsquiva = Input.GetButtonDown("Dash");
+
+        if(podeEsquivar && botaoEsquiva)
+        {
+            esquivaAtiva = true;
+            podeEsquivar = false;
+            TracoDaEsquiva.emitting = true;
+            direcaoEsquiva = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            if(direcaoEsquiva == Vector2.zero)
+            {
+                direcaoEsquiva = new Vector2(transform.localScale.x, 0);
+            }
+
+            StartCoroutine("PararEsquiva");
+        }
+
+        if (esquivaAtiva) {
+            rigidbody2D.velocity = direcaoEsquiva.normalized * velocidadeEsquiva;
+            return;
+        }
+
+        if (taNoChao) 
+        {
+            podeEsquivar = true;
+        }
+    }
+
+    public IEnumerator PararEsquiva()
+    {
+        yield return new WaitForSeconds(tempoEsquiva);
+        TracoDaEsquiva.emitting = false;
+        esquivaAtiva = false;
+    }
+
+    public void Mover()
+    {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if(!esquivaAtiva)
         rigidbody2D.velocity = new Vector2(inputHorizontal * movimentoJogador, rigidbody2D.velocity.y);
-    }        
+    }
 
-    if (Input.GetKey("space") && taNoChao == true) 
+    public void Pular() 
     {
-        rigidbody2D.AddForce(new Vector2(0f, forcaPuloJogador));
-        taNoChao = false;
+        if (Input.GetKey("space") && taNoChao == true) 
+        {
+            rigidbody2D.AddForce(new Vector2(0f, forcaPuloJogador));
+            taNoChao = false;
+        }
     }
-
-    if (Input.GetKey("left shift") && !estaEsquiva)
-    {
-        StartCoroutine("ExecutarEsquiva");
-    }
-
-    }
-
-public void OnCollisionEnter2D(Collision2D other) 
-{
-    if (other.gameObject.tag == "Chao") 
-    {
-        taNoChao = true;
-    }
-}
-
-public IEnumerator ExecutarEsquiva()
-{
-    estaEsquiva = true;
-    float tempoDecorrido = 0f;
-    while (tempoDecorrido < duracaoEsquiva)
-    {
-        rigidbody2D.velocity = new Vector2(velocidadeEsquiva, rigidbody2D.velocity.y);
-        tempoDecorrido += Time.deltaTime;
-        yield return null;
-    }
-    estaEsquiva = false;
-}
-
 }
 
